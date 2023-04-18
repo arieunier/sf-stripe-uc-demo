aba742d96c4032f9ef61e3b8f0b28d0176b4891b
Augustin Rieunier <augustin@stripe.com>
2023-04-18 17:14:06 +0200

    updated deployToOrg.sh script

--- deployToOrg.sh
+++ deployToOrg.sh
@@ -12,6 +12,7 @@ fi
 
 echo "##### WARNING ######"
 echo "before running this command, please make sure that : "
+echo "installed the latest SF unified CLI AND SFDX !! See -> https://developer.salesforce.com/docs/atlas.en-us.sfdx_cli_reference.meta/sfdx_cli_reference/cli_reference_unified.htm "
 echo "- you already installed the Stripe Universal Connector"
 echo "- you already installed the 2022 class package"
 echo "- you configured in the org Person account properly"
@@ -25,11 +26,13 @@ DEVHUBALIAS=$1
 echo "Creating Meta Data api Package"
 rm -rf mdapi_output_dir
 mkdir mdapi_output_dir
-sfdx force:source:convert -d mdapi_output_dir/ --packagename SFUCDemo
+#sfdx force:source:convert -d mdapi_output_dir/ --package-name SFUCDemo
+sf project convert source --root-dir ./force-app/ --output-dir mdapi_output_dir
 read -p "------------- Finished, type enter to continue " 
 
 echo "Sending Metadata Api Package to the $DEVHUBALIAS Organisation"
-sfdx force:mdapi:deploy -d mdapi_output_dir  -u $DEVHUBALIAS -w 3
+#sfdx force:mdapi:deploy -d mdapi_output_dir  -u $DEVHUBALIAS -w 3
+sf project deploy start --metadata-dir mdapi_output_dir --target-org $DEVHUBALIAS
 read -p "------------- If not finished, wait before hitting enter by checking status in Deployment Status on SF" 
 
 echo "Updating user permissions" 
